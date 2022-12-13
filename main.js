80ec1f129d7abbf2b6bcd712e0bae4c2aaffe4ff
abarna10303 <abarnas361@gmail.com>
2022-12-13 16:56:37 +0530

    Subraction

---
+++ main.js
@@ -0,0 +1,7 @@
+/*SUBRACTION PROGRAM*/
+var num1,num2,sub;
+num1=parseInt(prompt("Enter the First Number"));
+num2=parseInt(prompt("Enter the second Number"));
+sub=num1-num2;
+console.log("Subraction of Two number is : " +sub);
+
